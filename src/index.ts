163795ae442dcbca656d6e4e01564306f480b96f
bryansummers14 <briansummers@Admins-MacBook-Pro.local>
2019-01-24 08:14:54 -0700

    Extract db config, test build steps

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 import _fastify from "fastify";
 import Knex from "knex";
 
+import { dbConfig } from '../config';
+
 const knex = Knex({
   client: "mysql",
-  connection: {
-    host: "localhost",
-    user: "3form",
-    password: "3formusa",
-    database: "3formusa"
-  }
+  connection: dbConfig
 });
 
-const fastify = _fastify();
+const fastify = _fastify({
+  logger: true
+});
 
 const PORT = process.env.PORT || "3030";
 
@@ -29,7 +28,7 @@ fastify.get("/", async (request, reply) => {
 const start = async () => {
   try {
     await fastify.listen(PORT);
-    fastify.log.info(`server listening on ${fastify.server.address()}`);
+    fastify.log.info(`server listening on http://${(fastify.server.address() as any)['address']}:${(fastify.server.address() as any)['port']}`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
